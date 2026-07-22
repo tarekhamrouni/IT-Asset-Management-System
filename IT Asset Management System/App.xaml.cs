a027c0750fa98447e90b30fc5194e0d186189d17
Tarek Hamrouni <tareqking4667@gmail.com>
2026-07-22 19:18:30 +0200

    chore: create initial WPF project structure

---
+++ IT Asset Management System/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace IT_Asset_Management_System
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
